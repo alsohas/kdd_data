ddb6b727de29def36df3c8ca78db370b2129b24f
Abdullah Islam <abdullahfmislam@gmail.com>
2020-10-21 21:12:26 -0400

    switched to dask

--- python_src/main.py
+++ python_src/main.py
@@ -1,13 +1,9 @@
-import concurrent.futures
-import sys
-
-import pandas as pd
-import numpy as np
 import os
+from dask.diagnostics import ProgressBar
+import dask
+import dask.dataframe as dd
 
-from tqdm import tqdm
-
-data_folder = '../scripts/gps/'
+data_folder = '../gps/'
 out_folder = '../clean_gps/'
 
 
@@ -16,42 +12,28 @@ def csvfy():
     files = os.listdir(data_folder)
     cols = ['DRIVER_ID', 'TRIP_ID', 'TIMESTAMP', 'LONGITUDE', 'LATITUDE']
 
-    arg_tuples = []
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
-        for i, f in enumerate(files):
-            future = executor.submit(csvfy_aux, f, i, cols)
+    for i, f in enumerate(files):
+        csvfy_aux(f, i, cols)
 
 
 def csvfy_aux(f, i, cols):
-    df = pd.read_csv(os.path.join(data_folder, f), names=cols)
+    with ProgressBar():
+        df = dask.dataframe.read_csv(os.path.join(data_folder, f), names=cols)
     df = df.drop(columns=['DRIVER_ID'], axis=1)
-    df = df.sort_values(['TIMESTAMP'], ascending=True).groupby('TRIP_ID')
+    with ProgressBar():
+        df = df.set_index('TIMESTAMP').persist()
 
-    dfs = pd.DataFrame(columns=['TRIP_ID', 'TIME_DELTA', 'POLYLINE'])
+    with ProgressBar():
+        ndf = df.groupby('TRIP_ID').apply(lambda x: process_group(x.LONGITUDE, x.LATITUDE)).to_frame(
+            name='POLYLINE').reset_index()
 
-    pbar = tqdm(total=len(df), desc='Processing groups', position=i+1, leave=True)
-    count = 1
-    update_interval = 1000
-    for name, group in df:
-        _df = group.reset_index()
-        coords = []
-        times = []
-        for index, row in _df.iterrows():
-            coords.append([row['LONGITUDE'], row['LATITUDE']])
-            times.append(row['TIMESTAMP'])
-        times = np.diff(np.array(times))
-        times = sum(times) / max(len(times), 1)
-        dfs.loc[len(dfs)] = [name, times, coords]
-        count += 1
-        if not count % update_interval:
-            pbar.update(update_interval)
-            sys.stdout.flush()
-    pbar.update((count % update_interval) + 1)
-    sys.stdout.flush()
-    pbar.close()
+    with ProgressBar():
+        ndf.to_csv(os.path.join(out_folder, f), index=False)
 
-    dfs.to_csv(os.path.join(out_folder, f'{f}.csv'), index=False)
+
+def process_group(lng, lat):
+    lonlat = set(zip(lng, lat))
+    return lonlat
 
 
 if __name__ == '__main__':
